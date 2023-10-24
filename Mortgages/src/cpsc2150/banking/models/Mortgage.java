440fb2ef92b0190767190c0f97de755a5ee58353
CarelessWisp <isaiahpham04@gmail.com>
2023-10-24 10:47:42 -0400

    Update Mortgage.java

--- Mortgages/src/cpsc2150/banking/models/Mortgage.java
+++ Mortgages/src/cpsc2150/banking/models/Mortgage.java
@@ -2,15 +2,17 @@ package cpsc2150.banking.models;
 
 public class Mortgage extends AbsMortgage implements IMortgage
 {
-    private double homeCost = 0;
-    private double downPayment = 0;
-    private int years = 0;
-    private ICustomer customer;
+    private double payment = 0;
+    private double rate = 0;
+    private ICustomer Customer;
+    private double debtToIncomeRatio = 0;
+    private double principal = 0;
+    private int numberOfPayments = 0;
+    private double percentDown = 0;
 
-    Mortgage(double home, double down, int y, ICustomer cust) {
-        homeCost = home;
-        downPayment = down;
-        years = y;
-        customer = cust;
+    Mortgage(double homeCost, double downPayment, int years, ICustomer cust) {
+        principal = homeCost - downPayment;
     }
+
+    double getRate
 }
