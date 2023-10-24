9c3dfd41f7e62232d658ed1fe358283a4dbd5597
kayleepierce <143130896+kayleepierce@users.noreply.github.com>
2023-10-24 11:08:41 -0400

    Update Mortgage.java

--- Mortgages/src/cpsc2150/banking/models/Mortgage.java
+++ Mortgages/src/cpsc2150/banking/models/Mortgage.java
@@ -22,7 +22,7 @@ public class Mortgage extends AbsMortgage implements IMortgage
     double getRate
 
     public double getPrincipal() {
-        return homeCost - downPayment;
+        return principal;
     }
 
     public int getYears() {
