52c22f83f190bbe92fcff4e83ede084fcf50ae90
CarelessWisp <isaiahpham04@gmail.com>
2023-10-24 11:05:17 -0400

    Implemented constructor

--- Mortgages/src/cpsc2150/banking/models/Mortgage.java
+++ Mortgages/src/cpsc2150/banking/models/Mortgage.java
@@ -1,4 +1,5 @@
 package cpsc2150.banking.models;
+//import java.lang.Math;
 
 public class Mortgage extends AbsMortgage implements IMortgage
 {
@@ -11,8 +12,33 @@ public class Mortgage extends AbsMortgage implements IMortgage
     private double percentDown = 0;
 
     Mortgage(double homeCost, double downPayment, int years, ICustomer cust) {
+        Customer = cust;
         principal = homeCost - downPayment;
+        percentDown = downPayment/homeCost;
+        numberOfPayments = years * 12;
+
+        //Calculating the APR and then the interest rate
+        rate = .025;
+        rate = ((years<30) ? rate+0.005 : rate+.01);
+        rate = ((percentDown<0.20) ? rate+0.05 : rate);
+        double credit = cust.getCreditScore();
+        if (credit < 500) {
+            rate += 0.10;
+        }
+        else if (credit >= 500 && credit < 600) {
+            rate += 0.05;
+        }
+        else if (credit >= 600 && credit < 700) {
+            rate += 0.01;
+        }
+        else if (credit >= 700 && credit < 750) {
+            rate += 0.05;
+        }
+        rate /= 12; //divide APR by 12 to get monthly interest rate
+
+        payment = (rate*principal)/Math.pow((1-(1+rate)),-numberOfPayments);
+
+        debtToIncomeRatio = (cust.getMonthlyDebtPayments()+payment)/cust.getIncome();
     }
 
-    double getRate
 }
