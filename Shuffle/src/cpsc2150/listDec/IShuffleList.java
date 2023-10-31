e34cdc7af06c902620dff0f6ea3650c2710757a9
Ben Nazaruk <128210726+bn-clemson@users.noreply.github.com>
2023-10-31 09:54:14 -0400

    Implement IShuffleList shuffle()

--- Shuffle/src/cpsc2150/listDec/IShuffleList.java
+++ Shuffle/src/cpsc2150/listDec/IShuffleList.java
@@ -1,18 +1,31 @@
 package cpsc2150.listDec;
 
 import java.util.List;
+import java.util.Random;
 
 public interface IShuffleList<T> extends List<T>
 {
     // TODO: write contract
     default void shuffle(int swaps)
     {
-
+        Random rand = new Random();
+        for (int i = 0; i < swaps; i++)
+        {
+            // get two random indexes and swap them
+            int a = rand.nextInt(size());
+            int b;
+            // randomize b til unique from a
+            do
+            {
+                b = rand.nextInt(size());
+            }
+            while (b == a);
+            swap(a, b);
+        }
     }
 
     // TODO: write contract
     default void swap(int i, int j)
     {
-
     }
 }
