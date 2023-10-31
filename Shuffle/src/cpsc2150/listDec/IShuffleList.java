626c4d4aab43003d5707b9dd76c7e5904d89c0f4
Ben Nazaruk <128210726+bn-clemson@users.noreply.github.com>
2023-10-31 09:42:34 -0400

    Add empty IShuffleList methods

--- Shuffle/src/cpsc2150/listDec/IShuffleList.java
+++ Shuffle/src/cpsc2150/listDec/IShuffleList.java
@@ -1,4 +1,18 @@
 package cpsc2150.listDec;
 
-public interface IShuffleList {
+import java.util.List;
+
+public interface IShuffleList<T> extends List<T>
+{
+    // TODO: write contract
+    default void shuffle(int swaps)
+    {
+
+    }
+
+    // TODO: write contract
+    default void swap(int i, int j)
+    {
+
+    }
 }
