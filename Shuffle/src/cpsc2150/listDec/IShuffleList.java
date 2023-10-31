adbf344a47bcec6f494f895ba28f9b3b787f18bc
koreyb77 <koreyb@g.clemson.edu>
2023-10-31 10:04:50 -0400

    Contracts for default methods

--- Shuffle/src/cpsc2150/listDec/IShuffleList.java
+++ Shuffle/src/cpsc2150/listDec/IShuffleList.java
@@ -5,7 +5,15 @@ import java.util.Random;
 
 public interface IShuffleList<T> extends List<T>
 {
-    // TODO: write contract
+    /**
+     * Shuffles the elements in the list by randomly picking two positions and swapping them.
+     *
+     * @param swaps
+     *          the number of times to shuffle the list
+     *
+     * @pre swaps >= 0
+     * @post The elements in the list are randomly shuffled 'swaps' times.
+     */
     default void shuffle(int swaps)
     {
         Random rand = new Random();
@@ -24,7 +32,15 @@ public interface IShuffleList<T> extends List<T>
         }
     }
 
-    // TODO: write contract
+    /**
+     * Swaps the elements at positions 'i' and 'j' in the list.
+     *
+     * @param i the index of the first element to swap
+     * @param j the index of the second element to swap
+     *
+     * @pre 0 <= i < size() && 0 <= j < size()
+     * @post The elements at positions 'i' and 'j' have been swapped.
+     */
     default void swap(int i, int j)
     {
     }
